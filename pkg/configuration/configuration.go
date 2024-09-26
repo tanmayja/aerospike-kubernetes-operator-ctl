4ca5ce7becc8a9752fd5be53f1c98cc211094805
Tanmay Jain <tjain@aerospike.com>
2024-09-26 18:48:15 +0530

    updating go to 1.22 and fixing vul

--- pkg/configuration/configuration.go
+++ pkg/configuration/configuration.go
@@ -122,6 +122,7 @@ func (p *Parameters) ValidateNamespaces(ctx context.Context, namespaces []string
 			p.Logger.Warn(
 				fmt.Sprintf("namespaces %+v not present in cluster, skipping those namespaces",
 					nonExistentNs.UnsortedList()))
+
 			userNsSet = userNsSet.Difference(nonExistentNs)
 		}
 	}
